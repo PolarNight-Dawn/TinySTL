ad9442e0f65911ca0c3aab3799ee5d69abbcf930
polarnight <1813959165@qq.com>
2024-08-13 23:26:10 +0800

    Implement test_main.cpp

---
+++ Test/test_main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by polarnight on 24-8-11, 下午6:57.
+//
+
+#include <iostream>
+#include "test_vector.h"
+#include "test_list.h"
+
+int main() {
+
+  tinystl::vector_test();
+  tinystl::list_test();
+
+  return 0;
+}
